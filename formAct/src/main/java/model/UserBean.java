095e02b6ea09fbb94cb8704c82560880e45ca796
Fabio Picariello <fabio.picariello10@gmail.com>
2022-02-14 15:58:10 +0100

    Aggiunta UserBean, UserDS e file context.xml per la creazione contesto e datasource definition per il db

---
+++ formAct/src/main/java/model/UserBean.java
@@ -0,0 +1,146 @@
+package model;
+
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class UserBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	int id;
+	String email;
+	String password;
+	String name;
+	String surname;
+	String gender;
+	Date birthDate;
+	String country;
+
+	
+	
+	public UserBean()
+	{
+		id = 0;
+		email="";
+		password="";
+		name="";
+		surname="";
+		gender="";
+		birthDate= null;
+		country="";
+		
+		
+	}
+
+
+
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getSurname() {
+		return surname;
+	}
+
+
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "UserBean [id=" + id + ", email=" + email + ", password=" + password + ", name=" + name + ", surname="
+				+ surname + ", gender=" + gender + ", birthDate=" + birthDate + ", country=" + country + "]";
+	}
+
+	
+	
+	
+
+	
+	
+}
---
+++ formAct/src/main/java/model/UserDS.java
@@ -0,0 +1,202 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class UserDS  {
+	
+private static DataSource ds;
+    
+	static {
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+
+			ds = (DataSource) envCtx.lookup("jdbc/formactds");
+          
+		} catch (NamingException e) {
+			System.out.println("Error:" + e.getMessage());
+		}
+	}
+                                    
+	private static final String TABLE_NAME = "studente";
+	
+	// creazione id studente dinamico
+	public int nextId() throws SQLException {
+		
+		ArrayList<UserBean> users = (ArrayList<UserBean>) this.doRetrieveAll();
+		if(users.size()==0)
+			return 1;
+		int next = (users.get(users.size()-1).getId())+1;
+		
+		return next;
+
+	}
+
+	
+	public void doSave(Object bean) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		UserBean user = (UserBean) bean;
+		String insertSQL = "INSERT INTO " + UserDS.TABLE_NAME
+				+ " (IDSTUDENTE, EMAIL, PASSWORD, NOME, COGNOME, SESSO, DATANASCITA, PAESEORIGINE)"
+				+ " VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(insertSQL);
+			preparedStatement.setInt(1, this.nextId());
+			preparedStatement.setString(2, user.getEmail());
+			preparedStatement.setString(3, user.getPassword());
+			preparedStatement.setString(4, user.getName());
+			preparedStatement.setString(5, user.getSurname());
+			preparedStatement.setString(6, user.getGender());
+			preparedStatement.setDate(7, user.getBirthDate());
+			preparedStatement.setString(8, user.getCountry());
+			preparedStatement.executeUpdate();
+			connection.setAutoCommit(false);
+			connection.commit();
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+	}
+		
+	
+
+	
+	public boolean doDelete(int id) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		
+	
+         
+		int result = 0;
+
+		String deleteSQL = "DELETE FROM " + UserDS.TABLE_NAME + " WHERE IDSTUDENTE = ?";
+        
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(deleteSQL);
+			preparedStatement.setInt(1, id);
+
+			result = preparedStatement.executeUpdate();
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return (result != 0);
+	}
+	
+
+	
+	public Object doRetrieveByKey(int id) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		
+		
+		UserBean bean = new UserBean();
+        
+		String selectSQL = "SELECT * FROM " + UserDS.TABLE_NAME + " WHERE IDSTUDENTE = ?";
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+			preparedStatement.setInt(1, id);
+
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				bean.setId(rs.getInt("IDSTUDENTE"));
+				bean.setEmail(rs.getString("EMAIL"));
+				bean.setPassword(rs.getString("PASSWORD"));
+				bean.setName(rs.getString("NOME"));
+				bean.setSurname(rs.getString("COGNOME"));
+			    bean.setGender(rs.getString("SESSO"));
+	            bean.setBirthDate(rs.getDate("DATANASCITA"));
+	            bean.setCountry(rs.getString("PAESEORIGINE"));
+	       
+			    
+			}
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return bean;
+	}
+	
+
+	
+	public ArrayList<UserBean> doRetrieveAll() throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		ArrayList<UserBean> users = new ArrayList<UserBean>();
+
+		String selectSQL = "SELECT * FROM " + UserDS.TABLE_NAME;
+
+		if (users != null && !users.equals("")) {
+			selectSQL += " ORDER BY IDSTUDENTE";  // l' errore era qui , clausola order by non aveva un attributo corretto per la tabella studente.
+		}
+
+		try {
+			connection = ds.getConnection();
+			preparedStatement = connection.prepareStatement(selectSQL);
+
+			ResultSet rs = preparedStatement.executeQuery();
+
+			while (rs.next()) {
+				
+				UserBean bean = new UserBean();
+
+				bean.setId(rs.getInt("IDSTUDENTE"));
+				bean.setEmail(rs.getString("EMAIL"));
+				bean.setPassword(rs.getString("PASSWORD"));
+				bean.setName(rs.getString("NOME"));
+				bean.setSurname(rs.getString("COGNOME"));
+			    bean.setGender(rs.getString("SESSO"));
+	            bean.setBirthDate(rs.getDate("DATANASCITA"));
+	            bean.setCountry(rs.getString("PAESEORIGINE"));
+				users.add(bean);
+			}
+
+		} finally {
+			try {
+				if (preparedStatement != null)
+					preparedStatement.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return users;
+	}
+	}

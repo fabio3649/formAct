d4a841848fa95057292bebe903a4399640e8a1bc
Fabio Picariello <fabio.picariello10@gmail.com>
2022-02-20 19:06:40 +0100

    Modifiche ai Dao percorso formativo, iscrizione, interesse. disponibilità, formatre, studente;

--- formAct/src/main/it.formact/getionepf/control/CreazionePercorsoServlet.java
+++ formAct/src/main/it.formact/getionepf/control/CreazionePercorsoServlet.java
@@ -2,6 +2,11 @@ package getionepf.control;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,8 +15,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.dao.CategoriaDao;
+import model.dao.Disponibilit‡Dao;
 import model.dao.PercorsoFormativoDao;
 import model.entity.CategoriaEntity;
+import model.entity.Disponibilit‡Entity;
 import model.entity.PercorsoFormativoEntity;
 
 /**
@@ -45,22 +52,38 @@ public class CreazionePercorsoServlet extends HttpServlet {
 		// Dao e entity
 		PercorsoFormativoEntity p = new PercorsoFormativoEntity();
 		PercorsoFormativoDao dao = new PercorsoFormativoDao();
+		ArrayList<PercorsoFormativoEntity> percorsi = (ArrayList<PercorsoFormativoEntity>) new ArrayList<PercorsoFormativoEntity>();
 		CategoriaDao daoCat = new CategoriaDao();
 		CategoriaEntity c = new CategoriaEntity();
+		Disponibilit‡Dao daoDisp = new Disponibilit‡Dao();
+		Disponibilit‡Entity disp = new Disponibilit‡Entity();
+		PercorsoFormativoEntity id = new PercorsoFormativoEntity();
 		
 		
 		// recupero i parametri dal form
 		String nome = (String) request.getParameter("nome");
 		String descrizione = (String) request.getParameter("descrizione");
 		String categoria = request.getParameter("categoria");
+		String giorno = request.getParameter("giorno");
+		String orario = request.getParameter("orario");
+		//String [] input = orario.split(":");
 		
-		
+		LocalTime time = LocalTime.parse(orario);
+	
 		
 		String indice = (String) request.getParameter("indice");
 		int lezioni = Integer.parseInt(request.getParameter("lezioni"));
 		double costo = Double.parseDouble(request.getParameter("costo"));
 		
 		
+		
+		try {
+			p.setId(dao.nextId());
+			System.out.println("id percorso appena creato : " + p.getId());
+		} catch (SQLException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
 		p.setNome(nome);
 		p.setCategoria(Integer.parseInt(categoria));
 		p.setDescrizione(descrizione);
@@ -72,10 +95,25 @@ public class CreazionePercorsoServlet extends HttpServlet {
 		System.out.println(p.toString());		
 		try {
 			dao.doSave(p);
-			System.out.println("Inserimento percorso effettuato con successo\n");
+			
+			
+			System.out.println("Inserimento percorso effettuato con successo id: "+ p.getId() +  "\n");
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		
+		disp.setGiornoSettimana(giorno);
+		disp.setOrario(time);
+		disp.setStato(1);
+		disp.setIdPercorso(p.getId());
+		try {
+			daoDisp.doSave(disp);
+			System.out.println("Inserimento disponibilit‡ effettuato con successo Id: " + disp.getIdDisp() +"\n");
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
 		
 		
